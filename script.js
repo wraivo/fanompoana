3cf07d710b10fb6ed991b54949fdbec5ae0ac457
wraivo <wnraivo@gmail.com>
2022-09-16 10:31:48 +0300

    2 commit

--- script.js
+++ script.js
@@ -11,3 +11,31 @@ fetch("https://https://wraivo.github.io/fanompoana/membres.html")
     displayCocktail(data)
   })
   .catch((error) => console.error("FETCH ERROR:", error));
+  
+  function displayCocktail(data) {
+	const cocktail = data.services[0];
+	const cocktailDiv = document.getElementById("cocktail");
+	const cocktailName = cocktail.anarFohy;
+	const heading = document.createElement("h1");
+	heading.innerHTML = cocktailName;
+	cocktailDiv.appendChild(heading);}   
+	const cocktailIngredients = document.createElement("ul");
+	cocktailDiv.appendChild(cocktailIngredients);  
+  
+	const getIngredients = Object.keys(cocktail)
+    .filter(function (ingredient) {
+      return ingredient.indexOf("strIngredient") == 0;
+    })
+    .reduce(function (ingredients, ingredient) {
+      if (cocktail[ingredient] != null) {
+        ingredients[ingredient] = cocktail[ingredient];
+      }
+      return ingredients;
+    }, {});
+
+	for (let key in getIngredients) {
+    let value = getIngredients[key];
+    listItem = document.createElement("li");
+    listItem.innerHTML = value;
+    cocktailIngredients.appendChild(listItem);
+  }
