12d570bc4ef24099694b719a34d6629fd3044bb5
wraivo <94170634+wraivo@users.noreply.github.com>
2022-09-16 15:33:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,29 @@
-fetch("https://fanompo-6a67.restdb.io/rest/services")
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("NETWORK RESPONSE ERROR");
-    }
-  })
-  .then(data => {
-    console.log(data);
-    displayCocktail(data)
-  })
-  .catch((error) => console.error("FETCH ERROR:", error));
-  
-  function displayCocktail(data) {
-	const cocktail = data.services[0];
-	const cocktailDiv = document.getElementById("cocktail");
-	const cocktailName = cocktail.fohy;
-	const heading = document.createElement("h1");
-	heading.innerHTML = cocktailName;
-	cocktailDiv.appendChild(heading);}   
-	const cocktailIngredients = document.createElement("ul");
-	cocktailDiv.appendChild(cocktailIngredients);  
-  
-	const getIngredients = Object.keys(cocktail)
-    .filter(function (ingredient) {
-      return ingredient.indexOf("strIngredient") == 0;
-    })
-    .reduce(function (ingredients, ingredient) {
-      if (cocktail[ingredient] != null) {
-        ingredients[ingredient] = cocktail[ingredient];
-      }
-      return ingredients;
-    }, {});
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
 
-	for (let key in getIngredients) {
-    let value = getIngredients[key];
-    listItem = document.createElement("li");
-    listItem.innerHTML = value;
-    cocktailIngredients.appendChild(listItem);
-  }
-  
-  (function() {
-    var cors_api_host = 'cors-anywhere.herokuapp.com';
-    var cors_api_url = 'https://' + cors_api_host + '/';
-    var slice = [].slice;
-    var origin = window.location.protocol + '//' + window.location.host;
-    var open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function() {
-        var args = slice.call(arguments);
-        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-            targetOrigin[1] !== cors_api_host) {
-            args[1] = cors_api_url + args[1];
-        }
-        return open.apply(this, args);
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[YOUR-API-KEY-HERE]`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
     };
-})();
+
+    request.open("GET", url, true);
+    request.send();
+
+   function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  });
+});
